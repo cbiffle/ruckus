2324f3d12c032fae983344d7d759d66d9032b251
Cliff L. Biffle <code@cliffle.com>
2016-01-01 13:26:32 -0800

    testscene: reduced complexity.
    Intersecting the initial sphere with a half-space eliminates a
    discontinuity in the field toward the top of the negative sphere, and
    reduces complexity.

--- testscene.rkt
+++ testscene.rkt
@@ -6,15 +6,18 @@
 (define (scene)
   (for ([i 5])
     (translate (list (* i 100) 0 0)
-               (difference
-                 (sphere 100)
-                 (translate '[0 20 0] (sphere 100))
-                 )))
+               (bowl 100 20)))
 
   (translate '(0 125 0)
              (rotate '(0 1 0) 20
                      (cube 50)))
   )
 
+(define (bowl radius thickness)
+  (difference
+    (intersection
+      (sphere radius)
+      (half-space '[0 1 0] 0))
+    (translate `[0 ,thickness 0] (sphere radius))))
 
 (spheretrace scene)
