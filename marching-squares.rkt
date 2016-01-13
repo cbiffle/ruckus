72c77d341a33dc8c838e4f2bacbafd57e0158131
Cliff L. Biffle <code@cliffle.com>
2016-01-13 13:56:14 -0800

    outline: rewrite, refactor, clean up.
    It's very slightly faster now, and significantly easier to read.

---
+++ marching-squares.rkt
@@ -0,0 +1,235 @@
+#lang racket
+
+; An implementation of the 'marching squares' algorithm for isocontour
+; generation, with some optimizations for Lipschitz-continuous scalar fields.
+
+(provide outline->svg)
+
+
+(require "math.rkt")
+(require "compiler.rkt")
+(require "edsl.rkt")
+
+; ------------------------------------------------------------------------------
+; Utility functions that could likely live elsewhere.
+
+; Computes the smallest power of two equal to or larger than the positive
+; integer 'x'.
+(define (round-up-to-power-of-two x)
+  (arithmetic-shift 1 (integer-length (- x 1))))
+
+
+; ------------------------------------------------------------------------------
+; Occupancy test, centralized so it stays consistent.
+
+(define (occupied? v)
+  (negative? v))
+
+
+; ------------------------------------------------------------------------------
+; Representation of directed line segments, and operations upon them.
+;
+; A line segment is represented by two pairs of (x,y) coordinates, a start pair
+; and an end pair.
+
+(struct segment (sx sy ex ey) #:transparent)
+
+; Offsets a segment by x and y values.
+(define (segment-offset s x y)
+   (segment (+ x (segment-sx s))
+            (+ y (segment-sy s))
+            (+ x (segment-ex s))
+            (+ y (segment-ey s))))
+
+; Scales all components of a segment by a ratio.
+(define (segment-scale s x)
+  (segment (* x (segment-sx s))
+           (* x (segment-sy s))
+           (* x (segment-ex s))
+           (* x (segment-ey s))))
+
+
+; ------------------------------------------------------------------------------
+; Processing a single square using predetermined field values.  The location of
+; the square in world-space is immaterial; the segments produced are on the unit
+; square.  The placement of the samples:
+;
+;   d ------- c
+;   |         |
+;   |    e    |
+;   |         |
+;   a ------- b
+
+; Generates a numeric corner code, mapping the occupancy status of each of the
+; four corners to a bit in a four-bit integer.
+(define (corner-code a b c d)
+  (+ (if (occupied? a) 1 0)
+     (if (occupied? b) 2 0)
+     (if (occupied? c) 4 0)
+     (if (occupied? d) 8 0)))
+
+; Given two field samples, one above and one below zero, computes the position
+; of the zero point between them on a scale from 0 (first point) to 1 (second).
+(define (span p q)
+  ((0 . - . p) . / . (q . - . p)))
+  
+; Generates zero, one, or two contours for the square described by the five
+; sample points.
+(define (contours-for-square a b c d e out-f)
+  (case (corner-code a b c d)
+    [(0) void]
+    [(1) (out-f (segment 0 (span a d)
+                         (span a b) 0))]
+    [(2) (out-f (segment (span a b) 0
+                         1 (span b c)))]
+    [(3) (out-f (segment 0 (span a d)
+                         1 (span b c)))]
+    [(4) (out-f (segment 1 (span b c) 
+                         (span d c) 1))]
+    [(5)
+     (if (occupied? e)            
+       (begin
+         (out-f (segment 0 (span a d)
+                         (span d c) 1))
+         (out-f (segment 1 (span b c)
+                         (span a b) 0)))
+       (begin  ; not occupied
+         (out-f (segment 0 (span a d)
+                         (span a b) 0))
+         (out-f (segment 1 (span b c)
+                         (span d c) 1))))]
+
+    [(6) (out-f (segment (span a b) 0
+                         (span d c) 1))]
+    [(7) (out-f (segment 0 (span a d)
+                         (span d c) 1))]
+    [(8) (out-f (segment (span d c) 1
+                         0 (span a d)))]
+    [(9) (out-f (segment (span d c) 1
+                         (span a b) 0))]
+
+    [(10)
+     (if (occupied? e)
+       (begin
+         (out-f (segment (span a b) 0
+                         0 (span a d)))
+         (out-f (segment (span d c) 1
+                         1 (span b c))))
+       (begin ; not occupied
+         (out-f (segment (span a b) 0
+                         1 (span b c)))
+         (out-f (segment (span d c) 1
+                         0 (span a d)))))]
+
+    [(11) (out-f (segment (span d c) 1
+                          1 (span b c)))]
+    [(12) (out-f (segment 1 (span b c)
+                          0 (span a d)))]
+    [(13) (out-f (segment 1 (span b c)
+                          (span a b) 0))]
+    [(14) (out-f (segment (span a b) 0
+                          0 (span a d)))]
+    [(15) void]))
+
+
+; ------------------------------------------------------------------------------
+; Algorithm for locating all contour segments of a certain size within a region
+; of interest.
+;
+; This is similar to marching squares, but optimized for relatively sparse
+; contours defined by signed distance bound functions -- that is, implicit
+; surfaces whose potential function is Lipschitz-continuous for L=1.
+;
+; I have not completed a rigorous analysis of this algorithm, but empirically it
+; scales in the number of segments emitted, *not* the size of the region of
+; interest, which is critical.
+;
+; The algorithm is parameterized in terms of 'q', the quantum size.  Once space
+; is subdivided into a square of size 'q', the traditional marching-squares
+; contour finding method is applied to produce output.  This means that features
+; smaller than 'q' may be lost.  So it goes.
+;
+; 'out-f' is called with each discovered contour segment as its single argument.
+
+(define (find-contours q sdbf x y s out-f)
+  (let* ([s/2 (s . / . 2)]
+         ; Center sample point in world coordinates.
+         [world-center (vec3 (* q (+ x s/2))
+                             (* q (+ y s/2))
+                             0)]
+         ; Distance from center sample point to corner in units of 'q'.
+         [dist-to-corner (sqrt (+ (sqr s/2) (sqr s/2)))]
+         ; Lower bound of distance from center sample point to nearest surface,
+         ; in world units.
+         [center-sample (sdbf world-center)]
+         ; Lower bound of distance in units of 'q'.
+         [dist-to-surface (/ center-sample q)])
+    (when ((abs dist-to-surface) . <= . dist-to-corner)
+      ; This area of space is worth considering more closely.
+      (if (= s 1)
+        ; We are done subdividing space; look for segments.
+        (contours-for-square
+          (sdbf (vec3 (* q x) (* q y) 0))
+          (sdbf (vec3 (* q (add1 x)) (* q y) 0))
+          (sdbf (vec3 (* q (add1 x)) (* q (add1 y)) 0))
+          (sdbf (vec3 (* q x) (* q (add1 y)) 0))
+          center-sample
+          (lambda (seg)
+            (out-f (segment-scale (segment-offset seg x y) q))))
+        ; Otherwise, we must descend further.
+        (begin
+          (find-contours q sdbf    x         y      s/2 out-f)
+          (find-contours q sdbf (+ x s/2)    y      s/2 out-f)
+          (find-contours q sdbf (+ x s/2) (+ y s/2) s/2 out-f)
+          (find-contours q sdbf    x      (+ y s/2) s/2 out-f))))))
+
+
+; ------------------------------------------------------------------------------
+; SVG output and formatting.
+
+; Generates an SVG document structure and formatted group for line segments
+; generated by the execution of 'body'.
+(define (wrap-svg-document width height body)
+  (printf "<?xml version=\"1.0\" standalone=\"no\"?>~n")
+  (printf "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\"~n")
+  (printf "  \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">~n")
+  (printf "<svg version=\"1.1\"~n")
+  (printf "  width=\"~amm\" height=\"~amm\"~n"
+          (real->double-flonum width)
+          (real->double-flonum height))
+  (let ([left (- (width . / . 2))]
+        [top  (- (height . / . 2))])
+    (printf "  viewBox=\"~a ~a ~a ~a\"~n"
+            (real->double-flonum left)
+            (real->double-flonum top)
+            (real->double-flonum width)
+            (real->double-flonum height)))
+  (printf "  xmlns=\"http://www.w3.org/2000/svg\">~n")
+  (printf "<g stroke=\"black\" stroke-width=\"0.1\">~n")
+
+  (body)
+
+  (printf "</g></svg>~n"))
+
+; Generates a single line segment.
+(define (print-segment-as-svg-line s)
+  (printf "  <line x1=\"~a\" y1=\"~a\" x2=\"~a\" y2=\"~a\"/>~n"
+          (real->double-flonum (segment-sx s))
+          (real->double-flonum (segment-sy s))
+          (real->double-flonum (segment-ex s))
+          (real->double-flonum (segment-ey s))))
+
+(define (outline->svg gen size q)
+  (let* ([size2 (round-up-to-power-of-two (exact-ceiling (size . / . q)))]
+         [f (node->function (call-with-edsl-root gen))])
+    (wrap-svg-document
+      (size2 . * . q)
+      (size2 . * . q)
+      (thunk
+        (find-contours
+          q
+          f
+          (- (size2 . / . 2))
+          (- (size2 . / . 2))
+          size2
+          print-segment-as-svg-line)))))
--- outline.rkt
+++ outline.rkt
@@ -1,231 +1,27 @@
 #lang racket
 
-; Generates a 2D outline of an object's contours at its intersection with the
-; XY plane.  This is useful for e.g. laser-cutting 2D objects.
+; Command line driver for 2D outline generation.
 ; 
 ; Output is in SVG.
 ;
 ; For command-line usage, see the end of this file or run with '--help'.
 
-(require racket/flonum)
-(require "edsl.rkt")
-(require "compiler.rkt")
-(require "math.rkt")
+(require "marching-squares.rkt")
 (require "loader.rkt")
 
-(define (next-power-of-two x)
-  (arithmetic-shift 1 (integer-length (- x 1))))
-
-; ------------------------------------------------------------------------------
-; Line segment representation.  We represent line segments like SVG does:
-; using a start and end point.
-
-(struct segment (sx sy ex ey) #:transparent)
-
-; Offsets a segment by x and y values.
-(define (segment-offset s x y)
-  (segment (+ x (segment-sx s))
-           (+ y (segment-sy s))
-           (+ x (segment-ex s))
-           (+ y (segment-ey s))))
-
-; Scales all components of a segment by a ratio.
-(define (segment-scale s x)
-  (segment (* x (segment-sx s))
-           (* x (segment-sy s))
-           (* x (segment-ex s))
-           (* x (segment-ey s))))
-
-; Produces a single SVG line segment from a segment structure.
-(define (print-segment-as-svg-line out s)
-  (fprintf out
-           "<line x1=\"~a\" y1=\"~a\" x2=\"~a\" y2=\"~a\"/>~n"
-           (real->double-flonum (segment-sx s))
-           (real->double-flonum (segment-sy s))
-           (real->double-flonum (segment-ex s))
-           (real->double-flonum (segment-ey s))))
-
-
-; ------------------------------------------------------------------------------
-; Outline projection and generation.
-
-; Runs 'gen' as an EDSL root.  Evaluates the resulting geometry in the XY plane,
-; focusing on a 'width' x 'height' sized rectangle centered around the origin.
-; Geometry is diced up into 'granule'-sized squares and contours, if present,
-; are emitted to the port 'out' as SVG.
-;
-; (The actual area evaluated will be rounded up to a square power of two, so
-; don't use 'width' and 'height' in an attempt to clip geometry.)
-(define (sparse-project-outline out gen width height granule)
-  (let* ([side (next-power-of-two
-                 (exact-ceiling ((max width height) . / . granule)))]
-         [side/2 (side . / . 2)]
-         [f (node->function (call-with-edsl-root gen))])
-    (fprintf out "<?xml version=\"1.0\" standalone=\"no\"?>~n")
-    (fprintf out "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\"~n")
-    (fprintf out "  \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">~n")
-    (fprintf out "<svg version=\"1.1\"~n")
-    (let ([vb-left/top (real->double-flonum (* granule (- side/2)))]
-          [vb-width/height (real->double-flonum (* granule side))])
-      (fprintf out "  width=\"~amm\" height=\"~amm\"~n"
-               vb-width/height
-               vb-width/height)
-      (fprintf out "  viewBox=\"~a ~a ~a ~a\"~n"
-               vb-left/top
-               vb-left/top
-               vb-width/height
-               vb-width/height))
-      (fprintf out "  xmlns=\"~a\">~n"
-               "http://www.w3.org/2000/svg")
-    (fprintf out "<g stroke=\"black\" stroke-width=\"0.1\">~n")
-    (sparse-march f
-                  (- side/2)
-                  (- side/2)
-                  side
-                  granule
-                  (lambda (seg) (print-segment-as-svg-line out seg)))
-    (fprintf out "</g>~n")
-    (fprintf out "</svg>~n")))
-
-; Recursive sparse marching squares algorithm; implementation factor of
-; 'sparse-project-outline', above.
-;
-; Using a quantum (square size) 'q', evaluates the distance field evaluator 'f'
-; at points in the XY plane to discover contours.  Evaluation is limited to the
-; square (measured in quanta) starting at ('x', 'y') and measuring 's' quanta
-; on a side.  Contours are discoered as 'segment' structs; as each is found, the
-; 'out' function is applied to it.
-;
-; This algorithm uses kd-tree-style binary space partitioning to avoid
-; evaluating regions of space containing no contours.
-; 
-; The distance field evaluator 'f' must be Lipschitz continuous at L=1 for this
-; to work correctly.
-(define (sparse-march f x y s q out)
-  (let* ([s/2 (s . / . 2)]
-         ; Distance from sample point to corner in q units
-         [dist-to-corner (sqrt (+ (sqr s/2) (sqr s/2)))]
-         ; Distance from sample point to nearest surface, also in q units.
-         ; Since 'f' is natively in world units we have to adjust.
-         [dist-to-surface (/ (f (vec3 (* q (+ x s/2))
-                                      (* q (+ y s/2))
-                                      0))
-                             q)])
-    (when ((abs dist-to-surface) . <= . dist-to-corner)
-      (if (= s 1)
-        (for ([seg (in-list (marching-square f (* q x) (* q y) q))])
-          (out (segment-scale (segment-offset seg x y) q)))
-        (begin
-          (sparse-march f x y s/2 q out)
-          (sparse-march f (+ x s/2) y s/2 q out)
-          (sparse-march f x (+ y s/2) s/2 q out)
-          (sparse-march f (+ x s/2) (+ y s/2) s/2 q out))))))
-
-; Implementation factor of 'marching-square', below.  Given the evaluated field
-; levels at each of four corners of a square -- starting from the lower left and
-; moving counter-clockwise -- this produces a numeric index to the contour table
-; below.  (It is also, at the time of this writing, the same encoding used in
-; the examples on Wikipedia -- so you can refer to that article.)
-(define (corner-code a b c d)
-  (define (bit v x) (if (negative? x) v 0))
-
-  (+ (bit 1 a)
-     (bit 2 b)
-     (bit 4 c)
-     (bit 8 d)))
-
-; Single-square marching squares algorithm.  Produces zero, one, or two line
-; segments in a list describing the isocontours of distance field evaluator 'f'
-; within the square anchored at 'x' 'y' and measuring 'i' on a side.
-(define (marching-square f x y i)
-  ; Given two values, one above zero and one below, find the offset of the
-  ; root between them.
-  (define (span p q)
-    ((0 . - . p) . / . (q . - . p)))
-
-  (let* ([a (f (vec3    x       y    0))]
-         [b (f (vec3 (+ x i)    y    0))]
-         [c (f (vec3 (+ x i) (+ y i) 0))]
-         [d (f (vec3    x    (+ y i) 0))]
-
-         [code (corner-code a b c d)])
-    (case code
-      [(0) '()]
-      [(1) (list (segment 0 (span a d)
-                          (span a b) 0))]
-      [(2) (list (segment (span a b) 0
-                          1 (span b c)))]
-      [(3) (list (segment 0 (span a d)
-                          1 (span b c)))]
-      [(4) (list (segment 1 (span b c)
-                          (span d c) 1))]
-      [(5)
-       (let ([e (f (vec3 (+ x (/ i 2)) (+ y (/ i 2)) 0))])
-         (if (negative? e)
-           (list (segment 0 (span a d)
-                          (span d c) 1)
-                 (segment 1 (span b c)
-                          (span a b) 0))
-           (list (segment 0 (span a d)
-                          (span a b) 0)
-                 (segment 1 (span b c)
-                          (span d c) 1))))]
-
-      [(6) (list (segment (span a b) 0
-                          (span d c) 1))]
-      [(7) (list (segment 0 (span a d)
-                          (span d c) 1))]
-      [(8) (list (segment (span d c) 1
-                          0 (span a d)))]
-      [(9) (list (segment (span d c) 1
-                          (span a b) 0))]
-
-      [(10)
-       (let ([e (f (vec3 (+ x (/ i 2)) (+ y (/ i 2)) 0))])
-         (if (negative? e)
-           (list (segment (span a b) 0
-                          0 (span a d))
-                 (segment (span d c) 1
-                          1 (span b c)))
-           (list (segment (span a b) 0
-                          1 (span b c))
-                 (segment (span d c) 1
-                          0 (span a d)))))]
-
-      [(11) (list (segment (span d c) 1
-                           1 (span b c)))]
-      [(12) (list (segment 1 (span b c)
-                           0 (span a d)))]
-      [(13) (list (segment 1 (span b c)
-                           (span a b) 0))]
-      [(14) (list (segment (span a b) 0
-                           0 (span a d)))]
-      [(15) '()])))
-
-
-; ------------------------------------------------------------------------------
-; Command line driver.
-
 ; Knobs controlled from the command line, with default values:
-(define design-width 100)   ; width of rectangle to consider
-(define design-height 100)  ; height of rectangle to consider
-(define design-quantum 1)   ; size of quantum for marching squares
+(define design-size 128)    ; size of square ROI to consider
+(define design-quantum 1)   ; size of quantum for terminal contour
 
 (command-line
   #:program "outline"
   #:once-each
-  [("-d" "--dimension") w h
-                        "Clip to <w> x <h> rectangle around the origin."
-                        (begin
-                          (set! design-width (string->number w))
-                          (set! design-height (string->number h)))]
+  [("-d" "--dimension") s
+                        "Clip to <s> x <s> square around the origin."
+                        (set! design-size (string->number s))]
   [("-q" "--quantum") q
                       "Quantize space into <q>-sized chunks."
                       (set! design-quantum (string->number q))]
   #:args (path)
   (begin
-    (sparse-project-outline (current-output-port)
-                            (load-frep path)
-                            design-width
-                            design-height
-                            design-quantum)))
+    (outline->svg (load-frep path) design-size design-quantum)))
