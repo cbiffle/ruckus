7263393dc89988843d2f3ef3f75267f69a72554f
Cliff L. Biffle <code@cliffle.com>
2016-01-24 10:52:48 -0800

    out/surface: improve command line help.

--- out/surface.rkt
+++ out/surface.rkt
@@ -15,15 +15,29 @@
 
 (command-line
   #:program "surface"
+
+  #:usage-help
+  "Generates an STL triangulation of the surface of a design."
+
+  #:help-labels "--- options controlling output quality ---"
+
   #:once-each
-  [("-d" "--dimension") s
-                        "Clip to <s>-unit cube around the origin."
-                        (set! design-size (string->number s))]
-  [("-q" "--quantum") q
-                      "Quantize space into <q>-sized chunks."
-                      (set! design-quantum (string->number q))]
-  #:args (path out)
-  (call-with-output-file out #:exists 'replace
+  [("-d" "--dimension")
+   s
+   ("Focus on a cube around the origin, <s> units on a"
+    "side.  This is a hint to the algorithm, not a clipping region; parts of"
+    "the design outside the cube may be included in the output.")
+   (set! design-size (string->number s))]
+  [("-q" "--quantum")
+   q
+   ("Generate triangles no bigger than <q> units on"
+    "any axis.  This controls output quality; smaller <q> values make for"
+    "smoother surfaces, but also bigger files and more compute time.  In"
+    "general, halving the <q> value increases processing time 4x.")
+   (set! design-quantum (string->number q))]
+
+  #:args (design-path output-path)
+  (call-with-output-file output-path #:exists 'replace
     (lambda (f)
       (parameterize ([current-output-port f])
-        (surface->stl (load-frep path) design-size design-quantum)))))
+        (surface->stl (load-frep design-path) design-size design-quantum)))))
