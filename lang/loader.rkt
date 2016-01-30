de783e9068849f91244ed8db2d8bb3b132cc3bb4
Cliff L. Biffle <code@cliffle.com>
2016-01-30 08:48:41 -0800

    lang/loader: permit loading absolute paths.
    This was a case I hadn't tested in commit 05b587, when I switched design
    loading to #lang and dynamic-require: the latter requires a valid module
    path, and in Racket, an absolute filesystem path is *not* a valid module
    path.
    
    Relativizing it under the covers fixes this.

--- lang/loader.rkt
+++ lang/loader.rkt
@@ -9,5 +9,9 @@
 ; Loads the Racket module at 'path', reloading it if it has changed.  Returns
 ; the module's binding for 'design'.
 (define (load-frep path)
-  (dynamic-rerequire path)
-  (dynamic-require path 'design))
+  ; Racket won't permit modules to be loaded using absolute paths.  Instead, we
+  ; make a valid module path by generating the (potentially ridiculous)
+  ; relative path.
+  (let ([rel-path (find-relative-path (current-directory) path)])
+    (dynamic-rerequire rel-path)
+    (dynamic-require rel-path 'design)))
