11b77890fead40edf655f1bb87e7c12257e084d0
Cliff L. Biffle <code@cliffle.com>
2016-02-03 15:09:34 -0800

    lang/loader: more module path fixes. :-(

--- lang/loader.rkt
+++ lang/loader.rkt
@@ -12,6 +12,7 @@
   ; Racket won't permit modules to be loaded using absolute paths.  Instead, we
   ; make a valid module path by generating the (potentially ridiculous)
   ; relative path.
-  (let ([rel-path (find-relative-path (current-directory) path)])
+  (let* ([abs-path (simplify-path (path->complete-path path))]
+         [rel-path (find-relative-path (current-directory) abs-path)])
     (dynamic-rerequire rel-path)
     (dynamic-require `(submod ,rel-path ruckus-metadata) 'top-level-thunk)))
